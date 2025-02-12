b1af3c9e90262dd5c16c611bdb63ef92bcf559bb
paczur <paczur@Iron>
2025-02-12 22:21:56 +0100

    Increase size of braces

--- shared/.local/share/fonts/custom/script.sh
+++ shared/.local/share/fonts/custom/script.sh
@@ -7,13 +7,4 @@ fonttosfnt -o custom.otb custom.bdf
 mkfontdir
 mkfontscale
 fc-cache
-
-
-# cd bitmapfont2otb
-# . .venv/bin/activate
-# ./bitmapfont2otb ../custom.bdf ../custom.otb
-# deactivate
-# cd ..
-
-
-
+xset fp rehash
