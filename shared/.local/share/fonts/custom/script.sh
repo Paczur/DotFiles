c408b446bcbf08d9a6f796bdc9a889c9b1c1fa98
paczur <paczur@Iron>
2025-02-12 01:06:40 +0100

    W.I.P. on new font

---
+++ shared/.local/share/fonts/custom/script.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# rm custom.pcf.gz
+# bdftopcf custom.bdf > custom.pcf
+# gzip custom.pcf
+rm custom.otb
+fonttosfnt -o custom.otb custom.bdf
+mkfontdir
+mkfontscale
+fc-cache
+
+
+# cd bitmapfont2otb
+# . .venv/bin/activate
+# ./bitmapfont2otb ../custom.bdf ../custom.otb
+# deactivate
+# cd ..
+
+
+
