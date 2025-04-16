75cdda1a75c158ce9c8c2ab1b53193f88258c919
paczur <paczur@Silver>
2025-04-17 01:18:29 +0200

    Fix font metrics and generate pf2 version

--- shared/.local/share/fonts/custom/script.sh
+++ shared/.local/share/fonts/custom/script.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 rm custom.pcf.gz
+rm custom.pf2
 bdftopcf custom.bdf > custom.pcf
+grub-mkfont --output custom.pf2 custom.bdf
 gzip custom.pcf
 rm custom.otb
 fonttosfnt -o custom.otb custom.bdf
